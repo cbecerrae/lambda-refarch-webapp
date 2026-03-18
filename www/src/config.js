e8cce846c8683a826ae6809459d0ee02c94e60f2
Cristhian Becerra <cristhianbecerra99@gmail.com>
2026-03-17 21:00:00 -0500

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "s6k6adr4cep4ipcheaieitjef",     // CognitoClientID
+  "api_base_url": "https://z3cne5dmn1.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-cbecerra-lambda-refarch-webapp.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d1o1u0k9gc5y19.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
